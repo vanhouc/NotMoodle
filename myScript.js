9bea403c8c86a19388cb9d8c047e2c07ba207180
user <user@domain>
2013-12-08 22:41:31 +0000

    basic script added

---
+++ myScript.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	$('#login').mouseenter(function() {
+		$(this).css('font-weight','bold');
+	});
+	$('#login').mouseleave(function() {
+		$(this).css('font-weight','normal');
+	});
+});
