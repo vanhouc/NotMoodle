9d595a36882093c4f53b93e404828d83ffb22452
user <user@domain>
2013-12-09 03:49:58 +0000

    Working towards adding user login system

---
+++ login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="myStyle.css"></link>
+	</head>
+	<body>
+		<div class="wrapper">
+			<div class="titleblock">
+				<h1>NotMoodle.com</h1>
+				<h3>It's not Moodle!!!</h3>
+			</div>
+			<div class="navbar">
+			</div>
+		</div>
+	</body>
--- myScript.js
+++ myScript.js
@@ -1,8 +1,14 @@
 $(document).ready(function() {
 	$('#login').mouseenter(function() {
-		$(this).css('font-weight','bold');
+		$(this).css('font-weight', 'bold');
 	});
 	$('#login').mouseleave(function() {
-		$(this).css('font-weight','normal');
+		$(this).css('font-weight', 'normal');
+	});
+	$('#login').click(function() {
+		$(this).hide().after('<form id=\"loginform\" name=\"input\" onSubmit=\"checkLogin\" action=\"login.php\" method=\"post\">Username: <input id=\"loginname\" type=\"text\" name=\"user\"><br/>Password: <input id=\"inputpass\" type=\"password\" name=\"pass\"><br/><input type=\"submit\"></form>');
 	});
 });
+function checkLogin() {
+		$.post("login.php",)
+};
