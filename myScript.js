0dbcfa3001d20474ffd17c8de9b6d54922bc6b92
vanhouc <vanhouc@mail.nmc.edu>
2013-12-09 19:44:25 -0500

    Clean up form code

--- myScript.js
+++ myScript.js
@@ -6,7 +6,11 @@ $(document).ready(function() {
 		$(this).css('font-weight', 'normal');
 	});
 	$('#login').click(function() {
-		$(this).hide().after('<form id=\"loginform\" name=\"input\" onSubmit=\"checkLogin\" action=\"login.php\" method=\"post\">Username: <input id=\"loginname\" type=\"text\" name=\"user\"><br/>Password: <input id=\"inputpass\" type=\"password\" name=\"pass\"><br/><input type=\"submit\"></form>');
+		$(this).hide().after(
+			'<form id=\"loginform\" name=\"input\" onSubmit=\"checkLogin\" action=\"login.php\" method=\"post\">
+			Username: <input id=\"loginname\" type=\"text\" name=\"user\"><br/>
+			Password: <input id=\"inputpass\" type=\"password\" name=\"pass\"><br/>
+			<input type=\"submit\"></form>');
 	});
 });
 function checkLogin() {
