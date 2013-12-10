cb3631fb61b332a05ead1f7d45cfbeba38a86e8a
vanhouc <vanhouc@mail.nmc.edu>
2013-12-09 19:56:52 -0500

    going back to visual studio monaco

--- myScript.js
+++ myScript.js
@@ -7,7 +7,6 @@ $(document).ready(function() {
 	});
 	$('#login').click(function() {
 		$(this).hide().after(
-			'<form id=\"loginform\" name=\"input\" onSubmit=\"checkLogin\" action=\"login.php\" method=\"post\">Username: <input id=\"loginname\" type=\"text\" name=\"user\"><br/>Password: <input id=\"inputpass\" type=\"password\" name=\"pass\"><br/><input type=\"submit\"></form>');
 	});
 });
 function checkLogin() {
