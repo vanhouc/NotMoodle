c20b6479bdf37de7dc8723c1cbcb5b22fa6a1374
vanhouc <vanhouc@mail.nmc.edu>
2013-12-11 14:47:23 -0500

    Working on adding log-in via json

--- myScript.js
+++ myScript.js
@@ -1,3 +1,31 @@
+function validateLogin() {
+	if (checkLogin(document.getElementById("loginName").value, document.getElementById("loginPass").value)) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+function checkLogin(username, password) {
+	// var req = new XMLHttpRequest();
+	// req.open("POST", "")
+	// $.ajax({
+	// 	url: "Users.json",
+	// 	type: 'get',
+	// 	datatype: 'jsonp',
+	// 	success: function(result) {
+	// 		console.log("SUCCESS!!!"+"\n"+result);
+	// 		return true;
+	// 	},
+	// 	error: function(result) {
+	// 		console.log("Failure!");
+	// 		return false;
+	// 	}
+	// 	});
+	$.getJSON("Users.json", function(result) {
+		console.log(result);
+	});
+}
 $(document).ready(function() {
 	$('#login').mouseenter(function() {
 		$(this).css('font-weight', 'bold');
@@ -10,7 +38,5 @@ $(document).ready(function() {
 		$(this).hide();
 		$('#loginForm').show();
 	});
+
 });
-function checkLogin() {
-	
-};
