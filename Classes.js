1788c0c227d96768075b21d5e1385d9cf2871b92
vanhouc <vanhouc@mail.nmc.edu>
2013-12-11 20:01:47 -0500

    Created class data

---
+++ Classes.js
@@ -0,0 +1,42 @@
+{
+	"courses": [
+		{
+			"title": "CIT255",
+			"instructor": "Jeff Straw",
+			"assignments": [
+				{
+					"title": "Lorem ipsum",
+					"due": "12/12/2013",
+					"content": "Lorem ipsum dolor sit amet, vis dico utroque omittam at, pro ei adversarium neglegentur consectetuer. Singulis postulant mediocrem sit te, at meliore maiorum consequat mei, mucius deserunt vituperatoribus vim ne. Vis omnesque consulatu eu. In vis mollis labores, alia efficiendi te cum. Cu quis graeco nonumes mel, te ius laudem fuisset."
+				},
+				{
+					"title": "At temporibus",
+					"due": "12/13/2013",
+					"content": "At temporibus complectitur qui, eu indoctum pertinacia vel, pri postea utamur ea. Eos id putent semper mnesarchum, quo saepe rationibus et, ex duo salutandi scribentur complectitur. Dicta bonorum qui ad, minim elaboraret pro no, probo animal honestatis sit id. Numquam atomorum suavitate at sed, per euismod docendi evertitur cu. Quo no dico dicat disputationi, te sed legere ocurreret. Ad corpora iudicabit qui. No duo saperet invenire, ex everti omnium mea, no quidam tractatos gloriatur sea."
+				}
+			]
+		},
+		{
+			"title": "CIT248",
+			"instructor": "Gene Sauter",
+			"assignments": [
+				{
+					"title": "Sit ei"
+					"due": "12/13/2013",
+					"content": "Sit ei esse solet, et euripidis vulputate nam. Te his voluptaria quaerendum vituperatoribus. Id nonumy nostro eos, iuvaret facilisi qui et, te vix mazim admodum. Melius adolescens id eam, no cum quas primis sadipscing, tation utinam doming sed no. Legere lobortis consequat cum an."
+				}
+			]
+		},
+		{
+			"title": "CIT190",
+			"instructor": "Jeff Straw",
+			"assignments": [
+				{
+					"title": "Not Moodle",
+					"due": "12/11/2013",
+					"content": "Ius malorum vituperatoribus ei, nibh congue dicant ut cum, suas ubique per ex. Ex ius possim perpetua. Viris singulis theophrastus id pro, vim et detraxit constituam, mei ei veniam discere corrumpit. Eam diam vivendum eu, has ex iudico elaboraret. Vis laudem alienum te, ad volumus appellantur definitionem his."
+				}
+			]
+		}
+	]
+}
