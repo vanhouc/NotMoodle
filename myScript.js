6971d77592587b570f693b2770f4a48cef2f6668
vanhouc <vanhouc@mail.nmc.edu>
2013-12-12 15:18:51 -0500

    Didn't drill down enough

--- myScript.js
+++ myScript.js
@@ -2,10 +2,10 @@ var CurrentUser = null;
 function loginResponse(result) {
 	'use strict';
 	var x, loginName = document.getElementById("loginName").value, passName = document.getElementById("loginPass").value;
-		for (x = 0; x < result.length; x += 1) {
-			if (result[x] === loginName && result[x].password === passName) {
+		for (x = 0; x < result.users.length; x += 1) {
+			if (result.users[x].username === loginName && result.users[x].password === passName) {
 				//This function sets the global CurrentUser to the selected person and probably should be its own function
-				CurrentUser = result[x];
+				CurrentUser = result.users[x];
 				console.log(CurrentUser);
 			}
 		}
