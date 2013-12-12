a91707bef2c518bd2f860d11dd4a31fa012b30f3
vanhouc <vanhouc@mail.nmc.edu>
2013-12-11 23:48:02 -0500

    Yay for Async

--- myScript.js
+++ myScript.js
@@ -1,31 +1,23 @@
 function StateManager() {
 	'use strict';
 	this.CurrentUser = null;
+	this.loginValidated = function(userToAssign) {
+		this.CurrentUser = userToAssign;
+		SetupUserEnv();
+	};
 	this.checkLogin = function (username, password) {
-	if (this.CurrentUser === null) {
-		var e = this;
-		$.getJSON("Users.js", function (result, e) {
-			var x;
-			for (x in result) {
-				if (x === username && result[x].password === password && result.hasOwnProperty(x)) {
-					//This function sets the global CurrentUser to the selected person and probably should be its own function
-					e = result[x];
+		if (this.CurrentUser === null) {
+			$.getJSON("Users.js", function (result) {
+				var x;
+				for (x in result) {
+					if (x === username && result[x].password === password && result.hasOwnProperty(x)) {
+						//This function sets the global CurrentUser to the selected person and probably should be its own function
+						loginValidated(result[x]);
+					}
 				}
-			}
-		});
-	}
-		if (this.CurrentUser !== null) {
-			console.log("User successfully logged on as " + this.CurrentUser.fName);
-			return true;
+			});
 		}
-		console.log("User failed to log on");
-		// if (CurrentUser !== null) {
-		// 	console.log("DAFUQ?!?!?!");
-		// 	return true;
-		// }
-	console.log("This shouldn't be called while there is a CurrentUser");
-	return false;
-};
+	};
 }
 // stateManager.prototype.CurrentUser = null;
 // stateManager.prototype.checkLogin = function (username, password) {
@@ -53,7 +45,28 @@ function StateManager() {
 // 	console.log("This shouldn't be called while there is a CurrentUser");
 // 	return false;
 // };
-
+function SetupUserEnv(currStateManager) {
+	'use strict';
+	$("#loginForm").slideUp("slow", function() {
+	$("#login").html("Hello " + currStateManager.CurrentUser.fName).fadeIn("slow");
+	$.getJSON("Classes.js", function(results) {
+	var i, x, a, assList, today = new Date(), classList = results;
+	for (i = 0; i < currStateManager.getClasses().length; i+=1) {
+		for (x = 0; x < classList.length; x+=1) {
+			if (currStateManager.getClasses()[i] === classList[x].title) {
+				for (a = 0; a < classList[x].assignments.length; a+=1) {
+					assList = "<div id=" + classList[x].assignments[a].title + ">" +
+						"<p id=\"contentClass\">Class: " + classList[x].title + "</p>" +
+						"<p id=\"contentTitle\">Title: " + classList[x].assignments[a].title + "</p>" +
+						"<p id=\"contentDate\">Due On: " + classList[x].assignments[a].due + "	Remaining Time: " + ((today.getTime() - Date.getTime(classList[x].assignments[a].due)) / (24*60*60*1000)).toString() + "</p>" +
+						"<p id=\"contentBody\">" + classList[x].assignments[a].content + "</p></div>";
+				}
+			}
+		}
+	}
+	$("#mainSection").html(assList).slideDown("slow");
+});
+}
 $(document).ready(function() {
 	'use strict';
 	var currStateManager = new StateManager();
@@ -78,29 +91,6 @@ $(document).ready(function() {
 		}
 	});
 	$('#loginSubmit').click(function() {
-		if (currStateManager.checkLogin(
-			document.getElementById("loginName").value, 
-			document.getElementById("loginPass").value)) {
-			$("#loginForm").slideUp("slow", function() {
-				$("#login").html("Hello " + currStateManager.CurrentUser.fName).fadeIn("slow");
-			});
-			$.getJSON("Classes.js", function(results) {
-				var i, x, a, assList, today = new Date(), classList = results;
-				for (i = 0; i < currStateManager.getClasses().length; i+=1) {
-					for (x = 0; x < classList.length; x+=1) {
-						if (currStateManager.getClasses()[i] === classList[x].title) {
-							for (a = 0; a < classList[x].assignments.length; a+=1) {
-								assList = "<div id=" + classList[x].assignments[a].title + ">" +
-									"<p id=\"contentClass\">Class: " + classList[x].title + "</p>" +
-									"<p id=\"contentTitle\">Title: " + classList[x].assignments[a].title + "</p>" +
-									"<p id=\"contentDate\">Due On: " + classList[x].assignments[a].due + "	Remaining Time: " + ((today.getTime() - Date.getTime(classList[x].assignments[a].due)) / (24*60*60*1000)).toString() + "</p>" +
-									"<p id=\"contentBody\">" + classList[x].assignments[a].content + "</p></div>";
-							}
-						}
-					}
-				}
-				$("#mainSection").html(assList).slideDown("slow");
-			});
-		}
+		(currStateManager.checkLogin(document.getElementById("loginName").value, document.getElementById("loginPass").value))
 	});
 });
