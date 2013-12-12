47a1b78a1b1312e55ff3da02373a42b0c574c188
vanhouc <vanhouc@mail.nmc.edu>
2013-12-11 20:51:08 -0500

    Added function to populate content on login

--- myScript.js
+++ myScript.js
@@ -1,8 +1,9 @@
 function stateManager(userProfile) {
 	var CurrentUser;
 }
-loginManager.prototype.checkLogin = function (username, password) {
+stateManager.prototype.checkLogin = function (username, password) {
 	'use strict';
+	if (CurrentUser === null) {
 	$.getJSON("Users.js", function(result) {
 		var x;
 		for (x in result) {
@@ -11,13 +12,17 @@ loginManager.prototype.checkLogin = function (username, password) {
 				CurrentUser = result[x];
 			}
 		}
-		console.log("So close yet so far");
+		console.log("User not found or incorrect credentials");
 	});
+	if (CurrentUser !== null) return true;
+	else return false;
+	}
+	console.log("This shouldn\'t be called while there is a CurrentUser");
+	return false;
 }
 function validateLogin(aStateManager) {
 	'use strict';
 	if(aStateManager instanceof stateManager) {
-		checkLogin(document.getElementById("loginName").value, document.getElementById("loginPass").value, true);
 		if (CurrentUser !== null) {
 			$("#loginForm").slideUp("slow", function() {
 				$("#login").html("Hello " + CurrentUser.fName).fadeIn("slow");
@@ -40,10 +45,43 @@ $(document).ready(function() {
 	});
 	$('#loginForm').hide();
 	$('#login').click(function() {
-		$(this).hide();
-		$('#loginForm').show();
+		if (currStateManager.CurrentUser === null) {
+			$(this).hide();
+			$('#loginForm').show();
+		}
+		else {
+			if (var promptLogOut = confirm("Do you want to Log-Out?")) {
+				$("#mainSection").slideUp("slow").empty();
+			}
+		}
 	});
 	$('#loginSubmit').click(function() {
-		validateLogin(currStateManager);
+		if (currStateManager.checkLogin(
+			document.getElementById("loginName").value, 
+			document.getElementById("loginPass").value)) {
+			$("#loginForm").slideUp("slow", function() {
+				$("#login").html("Hello " + CurrentUser.fName).fadeIn("slow");
+			});
+			$.getJSON("Classes.js", function() {
+				var i, x, a, classList = responseTxt, assList, today = new Date();
+				for (i = 0; i < currStateManager.getClasses().length; i+=1) {
+					for (x = 0; x < classList.length; x+=1) {
+						if (currStateManager.getClasses()[i] === classList[x].title) {
+							for (a = 0, a < classList[x].assignments.length) {
+								assList += "<div id=" + classList[x].assignments[a].title + ">" +
+									"<p id=\"contentClass\">Class: " + classList[x].title + "</p>" +
+									"<p id=\"contentTitle\">Title: " + classList[x].assignments[a].title + "</p>" +
+									"<p id=\"contentDate\">Due On: " + classList[x].assignments[a].due + "	Remaining Time: " + ((today.getTime() - Date.getTime(classList[x].assignments[a].due)) / (24*60*60*1000)).toString() + "</p>" +
+									"<p id=\"contentBody\">" + classList[x].assignments[a].content + "</p></div>";
+							}
+						}
+					}
+				}
+				$("#mainSection").html(assList).slideDown("slow");
+				if (statusTxt === "error") {
+					console.log("Something has gone horribly wrong \n" + xhr.statusText);
+				}
+			});
+		}
 	});
 });
