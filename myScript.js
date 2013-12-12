fdea7c21517ce4821034f9d986176aad07197db8
vanhouc <vanhouc@mail.nmc.edu>
2013-12-11 19:01:09 -0500

    Moved login functions into stateManager object

--- myScript.js
+++ myScript.js
@@ -1,32 +1,37 @@
-var CurrentUser;
-function checkLogin(username, password, setCurrUser) {
+function stateManager(userProfile) {
+	var CurrentUser;
+}
+loginManager.prototype.checkLogin = function (username, password) {
 	'use strict';
 	$.getJSON("Users.js", function(result) {
 		var x;
 		for (x in result) {
 			if (x === username && result[x].password === password && result.hasOwnProperty(x)) {
 				//This function sets the global CurrentUser to the selected person and probably should be its own function
-				if (setCurrUser === true) {
-					CurrentUser = result[x];
-				}
+				CurrentUser = result[x];
 			}
 		}
 		console.log("So close yet so far");
 	});
 }
-function validateLogin() {
+function validateLogin(aStateManager) {
 	'use strict';
-	checkLogin(document.getElementById("loginName").value, document.getElementById("loginPass").value, true);
-	if (CurrentUser !== null) {
-		$("#loginForm").slideUp("slow", function() {
-			$("#login").html("Hello " + CurrentUser.fName).fadeIn("slow");
-		});
-		return true;
+	if(aStateManager instanceof stateManager) {
+		checkLogin(document.getElementById("loginName").value, document.getElementById("loginPass").value, true);
+		if (CurrentUser !== null) {
+			$("#loginForm").slideUp("slow", function() {
+				$("#login").html("Hello " + CurrentUser.fName).fadeIn("slow");
+			});
+			return true;
+		}
+		return false;
 	}
+	console.log(aStateManager + "Is not a valid stateManager");
 	return false;
 }
 $(document).ready(function() {
 	'use strict';
+	var currStateManager = new stateManager();
 	$('#login').mouseenter(function() {
 		$(this).css('font-weight', 'bold');
 	});
@@ -38,5 +43,7 @@ $(document).ready(function() {
 		$(this).hide();
 		$('#loginForm').show();
 	});
-
+	$('#loginSubmit').click(function() {
+		validateLogin(currStateManager);
+	});
 });
