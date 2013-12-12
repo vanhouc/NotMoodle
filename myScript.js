c586af292003cacc5cf3c762fe539bca663c5781
vanhouc <vanhouc@mail.nmc.edu>
2013-12-11 23:52:36 -0500

    Linted

--- myScript.js
+++ myScript.js
@@ -48,24 +48,25 @@ function StateManager() {
 function SetupUserEnv(currStateManager) {
 	'use strict';
 	$("#loginForm").slideUp("slow", function() {
-	$("#login").html("Hello " + currStateManager.CurrentUser.fName).fadeIn("slow");
-	$.getJSON("Classes.js", function(results) {
-	var i, x, a, assList, today = new Date(), classList = results;
-	for (i = 0; i < currStateManager.getClasses().length; i+=1) {
-		for (x = 0; x < classList.length; x+=1) {
-			if (currStateManager.getClasses()[i] === classList[x].title) {
-				for (a = 0; a < classList[x].assignments.length; a+=1) {
-					assList = "<div id=" + classList[x].assignments[a].title + ">" +
-						"<p id=\"contentClass\">Class: " + classList[x].title + "</p>" +
-						"<p id=\"contentTitle\">Title: " + classList[x].assignments[a].title + "</p>" +
-						"<p id=\"contentDate\">Due On: " + classList[x].assignments[a].due + "	Remaining Time: " + ((today.getTime() - Date.getTime(classList[x].assignments[a].due)) / (24*60*60*1000)).toString() + "</p>" +
-						"<p id=\"contentBody\">" + classList[x].assignments[a].content + "</p></div>";
+		$("#login").html("Hello " + currStateManager.CurrentUser.fName).fadeIn("slow");
+		$.getJSON("Classes.js", function(results) {
+		var i, x, a, assList, today = new Date(), classList = results;
+		for (i = 0; i < currStateManager.getClasses().length; i+=1) {
+			for (x = 0; x < classList.length; x+=1) {
+				if (currStateManager.getClasses()[i] === classList[x].title) {
+					for (a = 0; a < classList[x].assignments.length; a+=1) {
+						assList = "<div id=" + classList[x].assignments[a].title + ">" +
+							"<p id=\"contentClass\">Class: " + classList[x].title + "</p>" +
+							"<p id=\"contentTitle\">Title: " + classList[x].assignments[a].title + "</p>" +
+							"<p id=\"contentDate\">Due On: " + classList[x].assignments[a].due + "	Remaining Time: " + ((today.getTime() - Date.getTime(classList[x].assignments[a].due)) / (24*60*60*1000)).toString() + "</p>" +
+							"<p id=\"contentBody\">" + classList[x].assignments[a].content + "</p></div>";
+					}
 				}
 			}
 		}
-	}
-	$("#mainSection").html(assList).slideDown("slow");
-});
+		$("#mainSection").html(assList).slideDown("slow");
+		});
+	});
 }
 $(document).ready(function() {
 	'use strict';
@@ -91,6 +92,6 @@ $(document).ready(function() {
 		}
 	});
 	$('#loginSubmit').click(function() {
-		(currStateManager.checkLogin(document.getElementById("loginName").value, document.getElementById("loginPass").value))
+		(currStateManager.checkLogin(document.getElementById("loginName").value, document.getElementById("loginPass").value));
 	});
 });
